7d59697659e1fbf08baaacd7097e2b64294b8e5d
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-09-17 15:28:37 +0100

    adds add_passenger

--- bus.rb
+++ bus.rb
@@ -1,16 +1,25 @@
 class Bus
- attr_reader :route, :destination, :noise
+ attr_accessor :route, :destination, :noise, :passengers
 
 def initialize (route, destination, noise)
   @route = route
   @destination = destination
   @noise = noise
+  @passengers = []
+
 end
 
 def bus_noise(noise)
   return @bus.noise
 end
+
 def passengers_count()
-  return passengers.count
+  return @passengers.count()
 end
+
+def add_passenger(new_passenger)
+  @passengers.push(new_passenger)
+end
+
+
 end
--- bus_specs/bus_spec.rb
+++ bus_specs/bus_spec.rb
@@ -1,19 +1,26 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bus")
+require_relative("../person")
+
 
 class TestBus < Minitest::Test
-  def setup
+  def setup()
     @bus = Bus.new(22, "Ocean Terminal", "Brum brum")
-    @passengers = []
-end
-def test_bus_noise()
-assert_equal("Brum brum", @bus.noise)
-end
+    @passenger = Person.new("Harry Potter", 17)
+#    @new_passenger2 = Passenger.new("Luke Skywalker")
+  end
+  def test_bus_noise()
+    assert_equal("Brum brum", @bus.noise)
+  end
 
-def test_passengers_count()
-assert_equal(0, @passengers.count())
-end
+  def test_passengers_count()
+    assert_equal(0, @bus.passengers_count())
+  end
 
+  def test_add_passenger()
+    @bus.add_passenger(@passenger)
+    assert_equal(1, @bus.passengers_count)
+  end
 
 end
