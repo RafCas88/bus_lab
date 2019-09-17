ee6685e7bef409b167d79fe6590ec0c788e8cadc
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-09-17 14:18:00 +0100

    adds bus noise function

--- bus.rb
+++ bus.rb
@@ -1,8 +1,13 @@
 class Bus
-attr_reader
+ attr_reader :route, :destination, :noise
 
-def initialize (route, destination)
+def initialize (route, destination, noise)
   @route = route
   @destination = destination
+  @noise = noise
+end
+
+def bus_noise(noise)
+  return @bus.noise
 end
 end
--- bus_specs/bus_spec.rb
+++ bus_specs/bus_spec.rb
@@ -3,9 +3,11 @@ require("minitest/rg")
 require_relative("../bus")
 
 class TestBus < Minitest::Test
-
-def test_bus_route()
-assert_equal("22", @bus.route)
+  def setup
+    @bus = Bus.new(22, "Ocean Terminal", "Brum brum")
+end
+def test_bus_noise()
+assert_equal("Brum brum", @bus.noise)
 end
 
 
